1e3816d7693d7f2a5f4b527d35c868a7718bdca9
F74036386 <F74036386@mail.ncku.edu.tw>
2015-04-16 15:37:20 +0800

    master

--- hw1test.cpp
+++ hw1test.cpp
@@ -6,170 +6,117 @@ class Sudoku{
 	public:
 		void GiveQuestion();
 
-			void ReadIn();
-			void Solve();
-			void possibletest(int& locate);
-			void possible_ans_test(int& locate);
-			int darktest(int& locate);
-			void backtracking(int locate);
-			void output();		
-			int  questiontest();
-			private:
-			int question[144];
-			int answer[144];
-			bool darktable[16];
-			int answer_num;
-			bool possible_answer[144][10];
-			int possible_ans_num[144];
-			int out[144];
+		void ReadIn();
+		void Solve();
+		void possibletest(int& locate);
+		void possible_ans_test(int& locate);
+		int darktest(int& locate);
+		void backtracking(int locate);
+		void output();		
+		int  questiontest();
+	private:
+		int question[144];
+		int answer[144];
+		bool darktable[16];
+		int answer_num;
+		bool possible_answer[144][10];
+		int possible_ans_num[144];
+		int out[144];
 
-		};
-		int main (){
-			Sudoku ss;
-			ss.ReadIn();
-			//printf("start slove\n");
+};
+int main (){
+	Sudoku ss;
+	ss.ReadIn();
+	//printf("start slove\n");
 
-			ss.Solve();
-			//system("pause");
+	ss.Solve();
+	//system("pause");
 
-			return 0;
+	return 0;
+}
+
+
+void Sudoku::GiveQuestion(){
+printf(" 0 0 6 8 0 3 9 5 1 -1 -1 -1\n0 7 0 0 5 0 0 0 4 -1 -1 -1\n9 0 1 0 2 0 8 0 0 -1 -1 -1\n-1 -1 -1 1 0 2 0 0 0 9 0 6\n-1 -1 -1 0 8 0 1 0 0 0 2 0\n-1 -1 -1 0 0 6 0 0 0 0 7 1\n1 0 0 0 4 0 -1 -1 -1 0 9 0\n0 0 4 0 6 0 -1 -1 -1 1 0 7\n0 9 0 0 1 0 -1 -1 -1 0 8 0\n0 0 2 -1 -1 -1 0 4 0 5 0 0\n7 0 8 -1 -1 -1 0 0 9 0 0 2\n0 6 0 -1 -1 -1 0 1 0 7 0 0\n");
+return ;
+
+}
+
+
+
+
+void Sudoku::ReadIn(){
+	int num=0;
+	for(int i=0;i<144;i++){
+		cin>>num;
+		question[i]=num;
+
+	}
+
+	//output();
+
+}
+int Sudoku:: questiontest(){
+
+	for(int j=0;j<144;j++){
+		if(question[j]<=0)continue;
+
+
+		int x=j%12;
+		int y=j/12;
+		int a=x/3;
+		int b=y/3;
+
+		//cout<<"j="<<j<<endl;
+		for(int i=0;i<12;i++){
+			if((12*y+i)==j)continue;
+			if (question[12*y+i]==-1)continue;
+			if(question[12*y+i]==0)continue;
+			if(question[12*y+i]==question[j])return -1;    // row test;
+		}
+
+		//cout<<"j1="<<j<<endl;
+
+		for(int i=0;i<12;i++){
+			if((x+12*i)==j)continue;
+			if(question[x+12*i]==-1)continue;
+			if(question[x+12*i]==0)continue;
+			if(question[x+12*i]==question[j])return -1;       //clumn test;
 		}
 
 
-
-
-
-
-
-		void Sudoku::ReadIn(){
-			int num=0;
-			for(int i=0;i<144;i++){
-				cin>>num;
-				question[i]=num;
-
+		for(int i=0;i<3;i++){
+			for(int k=0;k<3;k++){
+				if(((36*b+3*a)+(i+12*k))==j)continue;
+				if(question[(36*b+3*a)+(i+12*k)]==-1)continue;
+				if(question[(36*b+3*a)+(i+12*k)]==0)continue;
+				if(question[(36*b+3*a)+(i+12*k)]==question[j])
+					return -1;
 			}
-
-			//output();
-
-		}
-		int Sudoku:: questiontest(){
-
-			for(int j=0;j<144;j++){
-				if(question[j]<=0)continue;
-
-
-				int x=j%12;
-				int y=j/12;
-				int a=x/3;
-				int b=y/3;
-
-				//cout<<"j="<<j<<endl;
-				for(int i=0;i<12;i++){
-					if((12*y+i)==j)continue;
-					if (question[12*y+i]==-1)continue;
-					if(question[12*y+i]==0)continue;
-					if(question[12*y+i]==question[j])return -1;    // row test;
-				}
-
-				//cout<<"j1="<<j<<endl;
-
-				for(int i=0;i<12;i++){
-					if((x+12*i)==j)continue;
-					if(question[x+12*i]==-1)continue;
-					if(question[x+12*i]==0)continue;
-					if(question[x+12*i]==question[j])return -1;       //clumn test;
-				}
-
-
-				for(int i=0;i<3;i++){
-					for(int k=0;k<3;k++){
-						if(((36*b+3*a)+(i+12*k))==j)continue;
-						if(question[(36*b+3*a)+(i+12*k)]==-1)continue;
-						if(question[(36*b+3*a)+(i+12*k)]==0)continue;
-						if(question[(36*b+3*a)+(i+12*k)]==question[j])
-							return -1;
-					}
-
-				}
-
-
-
-			}
-			return 0;
-
-
-
-		}
-
-		int Sudoku::darktest(int &locate){
-			int x=locate%12;
-			int y=locate/12;
-
-			int a=x/3;
-			int b=y/3;
-
-			for(int i=0;i<3;i++){
-				for(int j=0;j<3;j++){
-
-					if(question[(36*b+3*a)+(i+12*j)]!=-1){
-						return -1;
-
-					}
-
-
-				}
-
-
-			}
-			darktable[4*b+a]=1;
-			return 0;
 
 		}
 
 
 
-		void Sudoku::possibletest(int &locate){
-
-
-			int x=locate%12;
-			int y=locate/12;
-			int a=x/3;
-			int b=y/3;
-			if(question[locate]==0){
-				for(int j=0;j<10;j++){
-					possible_answer[locate][j]=0;
-
-				}
-			}
-			else {
-				for(int j=1;j<10;j++)
-					possible_answer[locate][j]=1;
-
-			}
+	}
+	return 0;
 
 
 
-			for(int i=0;i<12;i++){
-				if (question[12*y+i]==-1)continue;
-				int tem=question[12*y+i];
-				possible_answer[locate][tem]=1;    // row test;
-			}
+}
 
+int Sudoku::darktest(int &locate){
+	int x=locate%12;
+	int y=locate/12;
 
-			for(int i=0;i<12;i++){
-				if(question[x+12*i]==-1)continue;
-				int tem=question[x+12*i];
-				possible_answer[locate][tem]=1;      //clumn test;
-			}
+	int a=x/3;
+	int b=y/3;
 
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
 
-			for(int i=0;i<3;i++){
-				for(int j=0;j<3;j++){
-					if(question[(36*b+3*a)+(i+12*j)]==-1)continue;
-					int tem=question[(36*b+3*a)+(i+12*j)];
-					possible_answer[locate][tem]=1;
-
-				}
+			if(question[(36*b+3*a)+(i+12*j)]!=-1){
+				return -1;
 
 			}
 
@@ -177,267 +124,329 @@ class Sudoku{
 		}
 
 
-		void Sudoku::possible_ans_test(int& locate){
+	}
+	darktable[4*b+a]=1;
+	return 0;
 
-
-			int x=locate%12;
-			int y=locate/12;
-			int a=x/3;
-			int b=y/3;
-			if(answer[locate]==0&&question[locate]==0){
-				for(int j=0;j<10;j++){
-					possible_answer[locate][j]=0;
-
-				}
-			}
-			else {
-				for(int j=1;j<10;j++)
-					possible_answer[locate][j]=1;
-
-			}
+}
 
 
 
-			for(int i=0;i<12;i++){
-				if (answer[12*y+i]==-1)continue;
-				int tem=answer[12*y+i];
-				possible_answer[locate][tem]=1;    // row test;
-				if (question[12*y+i]==-1)continue;
-				int temp=question[12*y+i];
-				possible_answer[locate][temp]=1;
+void Sudoku::possibletest(int &locate){
+
+
+	int x=locate%12;
+	int y=locate/12;
+	int a=x/3;
+	int b=y/3;
+	if(question[locate]==0){
+		for(int j=0;j<10;j++){
+			possible_answer[locate][j]=0;
+
+		}
+	}
+	else {
+		for(int j=1;j<10;j++)
+			possible_answer[locate][j]=1;
+
+	}
 
 
 
-			}
+	for(int i=0;i<12;i++){
+		if (question[12*y+i]==-1)continue;
+		int tem=question[12*y+i];
+		possible_answer[locate][tem]=1;    // row test;
+	}
+
+
+	for(int i=0;i<12;i++){
+		if(question[x+12*i]==-1)continue;
+		int tem=question[x+12*i];
+		possible_answer[locate][tem]=1;      //clumn test;
+	}
+
+
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			if(question[(36*b+3*a)+(i+12*j)]==-1)continue;
+			int tem=question[(36*b+3*a)+(i+12*j)];
+			possible_answer[locate][tem]=1;
+
+		}
+
+	}
+
+
+}
+
+
+void Sudoku::possible_ans_test(int& locate){
+
+
+	int x=locate%12;
+	int y=locate/12;
+	int a=x/3;
+	int b=y/3;
+	if(answer[locate]==0&&question[locate]==0){
+		for(int j=0;j<10;j++){
+			possible_answer[locate][j]=0;
+
+		}
+	}
+	else {
+		for(int j=1;j<10;j++)
+			possible_answer[locate][j]=1;
+
+	}
 
 
 
-			for(int i=0;i<12;i++){
-				if(answer[x+12*i]==-1)continue;
-				int tem=answer[x+12*i];
-				possible_answer[locate][tem]=1;      //clumn test;
-
-				if(question[x+12*i]==-1)continue;
-				int temp=question[x+12*i];
-				possible_answer[locate][temp]=1;  
-
-
-			}
-
-
-			for(int i=0;i<3;i++){
-				for(int j=0;j<3;j++){
-					if(answer[(36*b+3*a)+(i+12*j)]==-1)continue;
-					int tem=answer[(36*b+3*a)+(i+12*j)];
-					possible_answer[locate][tem]=1;
-					if(question[(36*b+3*a)+(i+12*j)]==-1)continue;
-					int temp=question[(36*b+3*a)+(i+12*j)];
-					possible_answer[locate][temp]=1;
+	for(int i=0;i<12;i++){
+		if (answer[12*y+i]==-1)continue;
+		int tem=answer[12*y+i];
+		possible_answer[locate][tem]=1;    // row test;
+		if (question[12*y+i]==-1)continue;
+		int temp=question[12*y+i];
+		possible_answer[locate][temp]=1;
 
 
 
+	}
 
 
-				}
+
+	for(int i=0;i<12;i++){
+		if(answer[x+12*i]==-1)continue;
+		int tem=answer[x+12*i];
+		possible_answer[locate][tem]=1;      //clumn test;
+
+		if(question[x+12*i]==-1)continue;
+		int temp=question[x+12*i];
+		possible_answer[locate][temp]=1;  
 
 
-			}
+	}
+
+
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			if(answer[(36*b+3*a)+(i+12*j)]==-1)continue;
+			int tem=answer[(36*b+3*a)+(i+12*j)];
+			possible_answer[locate][tem]=1;
+			if(question[(36*b+3*a)+(i+12*j)]==-1)continue;
+			int temp=question[(36*b+3*a)+(i+12*j)];
+			possible_answer[locate][temp]=1;
+
+
+
 
 
 		}
 
-		void Sudoku::Solve(){
-			for(int i=0;i<16;i++){
-				darktable[i]=0;
-			}
 
-			for(int i=0;i<144;i++){      //test for dark;
-				if(question[i]==-1){
-					int tem=darktest(i);
-					if(tem==-1){
-						cout<<'0'<<endl;
-						return;
-					}
-				}
-			}
-			/* 
-			   for(int i=0;i<16;i++){
-			   if(i%4==0)cout<<endl;
-			   cout<<darktable[i]<<' ';
+	}
 
 
-			   } */  
-			//	cout<<'a'<<endl;
-			for(int i=0;i<4;i++){
-				int count1=0;
-				int count2=0;         //test for darktable
-				for(int j=0;j<4;j++){
+}
 
-					//printf("\naa\n");
-					//		cout<<darktable[i*4+j]<<endl<<darktable[i+4*j];
-					if(darktable[i*4+j]==1)count1++;
-					if(darktable[i+4*j]==1)count2++;
-				}
-				if(count1!=1||count2!=1){
-					cout<<'0'<<endl;
-					return ;
-				}	
-				//		printf("count1=%d\ncount2=%d\n",count1,count2);
+void Sudoku::Solve(){
+	for(int i=0;i<16;i++){
+		darktable[i]=0;
+	}
 
-
-
-			}
-
-			//	cout<<'b'<<endl;
-			/*
-			   for(int i=0;i<16;i++){
-			   if(i%4==0)cout<<endl;
-			   cout<<darktable[i]<<' ';
-			   }
-			 */
-
-
-			int iiii=questiontest();
-			if(iiii==-1){
+	for(int i=0;i<144;i++){      //test for dark;
+		if(question[i]==-1){
+			int tem=darktest(i);
+			if(tem==-1){
 				cout<<'0'<<endl;
-				return ;
+				return;
 			}
-			//	cout<<'c'<<endl;
-			int slonum=1;
-			while(slonum>0){     //test for smart
-
-				slonum=0;
-
-
-				for(int i=0;i<144;i++){     //smart 
-					possibletest(i);
-
-				}
+		}
+	}
+	/* 
+	   for(int i=0;i<16;i++){
+	   if(i%4==0)cout<<endl;
+	   cout<<darktable[i]<<' ';
 
 
+	   } */  
+	//	cout<<'a'<<endl;
+	for(int i=0;i<4;i++){
+		int count1=0;
+		int count2=0;         //test for darktable
+		for(int j=0;j<4;j++){
 
-
-				for(int i=0;i<144;i++){
-					possible_ans_num[i]=0;
-					for(int j=1;j<10;j++){
-						if(possible_answer[i][j]==0)possible_ans_num[i]++;
-					}
-					if(possible_ans_num[i]==1){
-						for(int j=1;j<10;j++){
-							if(possible_answer[i][j]==0){
-								question[i]=j;
-								slonum++;
-
-
-							}
-						}
-
-					}
-
-				}
-
-				/*			for(int i=0;i<144;i++){
-							if(i%12==0)cout<<endl;
-							cout<<possible_ans_num[i]<<' ';
-							}
-
-				 */
-
-			}
+			//printf("\naa\n");
+			//		cout<<darktable[i*4+j]<<endl<<darktable[i+4*j];
+			if(darktable[i*4+j]==1)count1++;
+			if(darktable[i+4*j]==1)count2++;
+		}
+		if(count1!=1||count2!=1){
+			cout<<'0'<<endl;
+			return ;
+		}	
+		//		printf("count1=%d\ncount2=%d\n",count1,count2);
 
 
 
+	}
 
-			for(int i=0;i<144;i++){
-				answer[i]=question[i];}
-			//	printf("\nbeforback\n");
-			backtracking(143);
-			if(answer_num==1){
-				cout<<'1'<<endl;
-				output();}
-			if(answer_num==0)cout<<'0'<<endl;
-			if(answer_num>1)cout<<'2'<<endl;
+	//	cout<<'b'<<endl;
+	/*
+	   for(int i=0;i<16;i++){
+	   if(i%4==0)cout<<endl;
+	   cout<<darktable[i]<<' ';
+	   }
+	 */
 
-			return;
+
+	int iiii=questiontest();
+	if(iiii==-1){
+		cout<<'0'<<endl;
+		return ;
+	}
+
+
+
+
+
+	//	cout<<'c'<<endl;
+	int slonum=1;
+	while(slonum>0){     //test for smart
+
+		slonum=0;
+
+
+		for(int i=0;i<144;i++){     //smart 
+			possibletest(i);
 
 		}
 
-		void Sudoku::backtracking(int locate){
-			if(locate==0){
-				if(question[locate]!=0){
-					answer[locate]=question[locate];
-					answer_num++;
-					//		if(answer_num>=2)return;
 
+
+
+		for(int i=0;i<144;i++){
+			possible_ans_num[i]=0;
+			for(int j=1;j<10;j++){
+				if(possible_answer[i][j]==0)possible_ans_num[i]++;
+			}
+			if(possible_ans_num[i]==1){
+				for(int j=1;j<10;j++){
+					if(possible_answer[i][j]==0){
+						question[i]=j;
+						slonum++;
+
+
+					}
+				}
+
+			}
+
+		}
+
+		
+		 
+
+	}
+
+
+answer_num=0;
+
+	for(int i=0;i<144;i++){
+		answer[i]=question[i];}
+	//	printf("\nbeforback\n");
+	backtracking(143);
+	if(answer_num==1){
+		cout<<'1'<<endl;
+		output();}
+	if(answer_num==0)cout<<'0'<<endl;
+	if(answer_num>1)cout<<'2'<<endl;
+
+	return;
+
+}
+
+
+
+
+
+void Sudoku::backtracking(int locate){
+	if(locate==0){
+		if(question[locate]!=0){
+			answer[locate]=question[locate];
+			answer_num++;
+			if(answer_num>=2)return;
+
+			for(int i=0;i<144;i++){
+				out[i]=answer[i];
+			}
+			//	output();
+
+
+
+		}
+
+		else if(question[locate]==0){
+			possible_ans_test(locate);
+
+			for(int i=1;i<10;i++){
+
+				if(possible_answer[locate][i]==0){
+					answer[locate]=i;
+					answer_num++;
+				//	cout<<answer_num<<endl;
+					if(answer_num>=2)return ;
 					for(int i=0;i<144;i++){
+
 						out[i]=answer[i];
 					}
-					output();
-
+				//		output();
 
 
 				}
 
-				else if(question[locate]==0){
-					possible_ans_test(locate);
-
-					for(int i=1;i<10;i++){
-
-						if(possible_answer[locate][i]==0){
-							answer[locate]=i;
-							answer_num++;
-							cout<<answer_num<<endl;
-							//if(answer_num>=2)return ;
-							for(int i=0;i<144;i++){
-
-								out[i]=answer[i];
-							}
-							output();
-
-
-						}
-
-					}
-					answer[locate]=0;
-
-				}
 			}
+			answer[locate]=0;
+
+		}
+	}
 
 
-			else if(question[locate]!=0){
-				answer[locate]=question[locate];
+	else if(question[locate]!=0){
+		answer[locate]=question[locate];
+		backtracking(locate-1);
+	}
+	else if(question[locate]==0){
+		possible_ans_test(locate);
+
+		for(int i=1;i<10;i++){
+
+			if(possible_answer[locate][i]==0){
+				answer[locate]=i;
 				backtracking(locate-1);
 			}
-			else if(question[locate]==0){
-				possible_ans_test(locate);
-
-				for(int i=1;i<10;i++){
-
-					if(possible_answer[locate][i]==0){
-						answer[locate]=i;
-						backtracking(locate-1);
-					}
-					//			if(answer_num>=2)return;
-
-				}
-				answer[locate]=0;
-
-
-			}
+			if(answer_num>=2)return;
 
 		}
+		answer[locate]=0;
+
+
+	}
+
+}
 
 
 
 
-		void Sudoku::output(){
-			for( int i=0;i<144;i++){
-				if(i%12==0)cout<<endl;
-				cout<<out[i]<<' ';
+void Sudoku::output(){
+	for( int i=0;i<144;i++){
+		if(i%12==0)cout<<endl;
+		cout<<out[i]<<' ';
 
 
-			}
-			cout<<endl;
+	}
+	cout<<endl;
 
-		}
+}
 
